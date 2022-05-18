e6203fe941758a11a7d3d5618239446785fd0f79
Philip Efem <pefemjnr@gmail.com>
2022-05-18 06:05:17 +0100

    initial commit, module/function template

---
+++ questions.py
@@ -0,0 +1,18 @@
+import csv
+
+
+# functions handling each question
+
+def question_one():
+    pass
+
+
+def question_two():
+    pass
+
+
+def question_three():
+    pass
+
+def question_four():
+    pass
---
+++ sections.py
@@ -0,0 +1,25 @@
+
+
+# functions handling question sections.
+
+
+def q1_section_a():
+    pass
+
+def q1_section_b():
+    pass
+
+
+def q2_section_a():
+    pass
+
+def q2_section_b():
+    pass
+
+
+def q3_section_a():
+    pass
+
+
+def q4_section_a():
+    pass
